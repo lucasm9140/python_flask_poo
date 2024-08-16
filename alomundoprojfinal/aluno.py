1ca739843fe8efa4f9fbad7fbf55110604f7ca27
lucasm9140 <lucas14fdk@gmail.com>
2024-08-16 02:23:23 -0300

    att e criando uma tabela times
    atualizações e criando uma tabela para  a lista de times com mais vitorias

---
+++ alomundoprojfinal/aluno.py
@@ -0,0 +1,6 @@
+class Aluno():
+    def __init__(self,ra,nome,renda_familiar,tempo_estudo):
+        self.ra = ra
+        self.nome = nome
+        self.renda_familiar = renda_familiar
+        self.tempo_estudo = tempo_estudo
